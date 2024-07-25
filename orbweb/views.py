7810de4780a44a7dd214c4e1b5d8d6c66c65d88d
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-07-25 16:07:19 -0300

    commit

--- orbweb/views.py
+++ orbweb/views.py
@@ -3,9 +3,11 @@ from .models import UserPost, DiscussionTopic
 
 def home(request):
     posts = UserPost.objects.all().order_by('-id')
+    top_disc_posts = DiscussionTopic.objects.all().order_by('-comments')[:3]
 
     return render(request, 'orbweb/pages/home.html', context={
         'posts': posts,
+        'disc_posts': top_disc_posts,
     })
 
 def discussion(request):
