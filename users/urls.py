681fdcd411d1996387ac4bda1da9d50164eb22ca
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-07-28 21:08:44 -0300

    forms

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import path
 
+app_name = 'user'
+
 urlpatterns = [
     path('signin/', views.signin, name='signin'),
+    path('signin/create/', views.signin_create, name='create'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
+from .forms import RegisterForm
 
 def signin(request):
-    return render(request, 'users/pages/signin.html')
+    user_form_data = request.session.get('user_form_data')
+    forms = RegisterForm(user_form_data)
+    return render(request, 'users/pages/signin.html', context={
+        'forms': forms,
+    })
+
+def signin_create(request):
+    if not request.POST:
+        raise Http404()
+    
+    POST = request.POST
+    request.session['user_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('user:signin')
